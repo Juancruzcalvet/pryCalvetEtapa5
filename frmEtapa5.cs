7a862ebad3ceda3201aa0c01f87e2480b49903c8
Juan <juancruzcalvetw@gmail.com>
2024-05-13 01:41:09 -0300

    Agregar archivos de proyecto.

---
+++ frmEtapa5.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCalvetEtapa5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        clsVehiculos objAuto;
+        clsVehiculos objAvion; 
+        clsVehiculos objBarco;
+        bool Auto = false;
+        bool Avion = false;
+        bool Barco = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objAuto = new clsVehiculos();
+            objAvion = new clsVehiculos(); 
+            objBarco = new clsVehiculos();
+
+            objAuto.CrearAuto();
+            objAvion.CrearAvion();  
+            objBarco.CrearBarco();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            Random random = new Random(); //Numero que esta cambiando constantemente
+            int Numero = random.Next(1, 4); //"Numero" toma el valor en ese instante y lo guarda
+
+            switch (Numero)
+            {
+                case 1:
+                    objAuto.CrearAuto();
+                    objAuto.pctAuto.Location = new Point(250, 50);
+                    Controls.Add(objAuto.pctAuto);
+                    Auto = true;
+                    if (Avion == true)
+                    {
+                        objAvion.pctAvion.Dispose();
+                        Avion = false;
+                    }
+                    if (Barco == true)
+                    {
+                        objBarco.pctBarco.Dispose();
+                        Barco = false;
+                    }
+                    break;
+
+                case 2:
+                    objAvion.CrearAvion();
+                    objAvion.pctAvion.Location = new Point(250, 50);
+                    Controls.Add(objAvion.pctAvion);
+                    Avion = true;
+                    if (Auto == true)
+                    {
+                        objAuto.pctAuto.Dispose();
+                        Auto = false;
+                    }
+                    if (Barco == true)
+                    {
+                        objBarco.pctBarco.Dispose();
+                        Barco = true;
+                    }
+                    break;
+
+                case 3:
+                    objBarco.CrearBarco();
+                    objBarco.pctBarco.Location = new Point(250, 50);
+                    Controls.Add(objBarco.pctBarco);
+                    Barco = true;
+                    if (Avion == true)
+                    {
+                        objAvion.pctAvion.Dispose();
+                        Avion = false;
+                    }
+                    if (Auto == true)
+                    {
+                        objAuto.pctAuto.Dispose();
+                        Auto = false;
+                    }
+                    break;
+            }
+        }
+    }
+}
